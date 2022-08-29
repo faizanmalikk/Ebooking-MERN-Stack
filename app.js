5617d2e7aaf1490697b7873c4bf76ce6d3c3bc0f
faizanmalykk <faizanjamil656@gmail.com>
2022-08-29 20:40:48 +0500

    updated

--- app.js
+++ app.js
@@ -42,10 +42,10 @@ app.use('/api' , user)
 app.use('/api' , room)
 
 
-app.use(express.static(path.join(__dirname,'../frontend/build')))
+app.use(express.static(path.join(__dirname,'./frontend/build')))
 
 app.get('*',(req,res)=>{
-  res.sendFile(path.resolve(__dirname,'../frontend/build/index.html'))
+  res.sendFile(path.resolve(__dirname,'./frontend/build/index.html'))
 })
 
 
